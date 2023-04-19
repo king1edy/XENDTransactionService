ca576b1b1c3f2baa198466ce1024c4b496f1bfc1
Godswill David <davidgodswill@gmail.com>
2023-04-19 11:53:05 +0100

    Add project files.

---
+++ src/TemplateDDD.Core/Utility/ENV.cs
@@ -0,0 +1,7 @@
+﻿namespace TemplateDDD.Core.Utility
+{
+    public class ENV
+    {
+        public static readonly string INTERSWITCH_WEBHOOK = System.Environment.GetEnvironmentVariable("INTERSWITCH_WEBHOOK", EnvironmentVariableTarget.Machine);
+    }
+}
